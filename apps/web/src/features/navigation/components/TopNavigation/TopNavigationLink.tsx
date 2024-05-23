d90c27a9cfd5321685af7df0dd4d01f144ae420c
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-05-24 01:26:37 +0900

    feat: ✨ (`<TopNavigationLinkFallback>`) `useSearchParams`を用いないフォールバック用のものを追加した。  (#5)
    - `<Suspanse>`で囲われていない部分ツリーで`useSearchParams`を用いると、ビルド時に警告文が出力されるため。参照: https://github.com/ReoHakase/simple-resas-app/pull/7#issuecomment-2127436035

--- apps/web/src/features/navigation/components/TopNavigation/TopNavigationLink.tsx
+++ apps/web/src/features/navigation/components/TopNavigation/TopNavigationLink.tsx
@@ -51,6 +51,85 @@ export type TopNavigationLinkProps = LinkProps &
     children: ReactNode;
   };
 
+/**
+ * 指定された href が現在のパスと一致しているかどうかを判定するカスタムフックです。
+ *
+ * @param href - リンク先のパス
+ * @returns 現在のパスと href が一致している場合は true、そうでない場合は false
+ */
+const useIsBeingOpened = (href: string): boolean => {
+  // 現在のパスを取得します。パスは「/」で始まります。クエリパラメータは無視されます。
+  // @see https://nextjs.org/docs/app/api-reference/functions/use-pathname
+  const currentPath = usePathname(); // 例: `/docs/works/shelfree`
+
+  // 現在のパスと href が同じかどうかをチェックします。
+  const isBeingOpened = useMemo(() => currentPath === href.toString(), [currentPath, href]);
+
+  return isBeingOpened;
+};
+
+/**
+ * 指定された href に現在のクエリパラメータを保持したままの URL を生成します。
+ * @param href 保持するクエリパラメータを含めたい href
+ * @returns 現在のクエリパラメータを保持したままの URL
+ */
+const useHrefWithPreservedSearchParams = (href: string): string => {
+  // 現在のクエリパラメータを取得します。
+  // @see https://nextjs.org/docs/app/api-reference/functions/use-search-params
+  const searchParams = useSearchParams();
+
+  const hrefWithPreservedSearchParams = useMemo<string>(() => {
+    const searchParamsString = new URLSearchParams(searchParams).toString();
+    const encodedSearchParamsString = searchParamsString.replace(/%2C/g, ',');
+    return encodedSearchParamsString ? `${href}?${encodedSearchParamsString}` : href.toString();
+  }, [href, searchParams]);
+
+  return hrefWithPreservedSearchParams;
+};
+
+/**
+ * ナビゲーションバー内のリンクを表すコンポーネントです。
+ * Suspenseのfallbackで使用するための、useSearchParamsを使わないバージョンです。
+ * @see https://github.com/ReoHakase/simple-resas-app/pull/7#issuecomment-2127436035
+ *
+ * @param href - リンク先のURL
+ * @param className - 追加のクラス名
+ * @param selected - リンクが選択されているかどうか
+ * @returns The rendered link component.
+ */
+export const TopNavigationLinkFallback = ({
+  href,
+  className,
+  selected,
+  children,
+  ...props
+}: TopNavigationLinkProps): ReactNode => {
+  const isBeingOpened = useIsBeingOpened(href.toString());
+
+  const link = topNavigationLinkRecipe({
+    selected: selected || isBeingOpened,
+  });
+
+  return (
+    <Link
+      href={href}
+      className={cx(link, className)}
+      aria-current={selected || isBeingOpened ? 'page' : undefined}
+      {...props}
+    >
+      {children}
+    </Link>
+  );
+};
+
+/**
+ * ナビゲーションバー内のリンクを表すコンポーネントです。
+ *
+ * @param href - リンク先のURL
+ * @param className - 追加のクラス名
+ * @param selected - リンクが選択されているかどうか
+ * @returns The rendered link component.
+ */
 export const TopNavigationLink = ({
   href,
   className,
@@ -58,21 +137,8 @@ export const TopNavigationLink = ({
   children,
   ...props
 }: TopNavigationLinkProps): ReactNode => {
-  // 現在のパスを取得します。パスは「/」で始まります。クエリパラメータは無視されます。
-  // @see https://nextjs.org/docs/app/api-reference/functions/use-pathname
-  const currentPath = usePathname(); // 例: `/docs/works/shelfree`
-  // 現在のパスとhrefが同じかどうかをチェックします。
-  const isBeingOpened = useMemo(() => currentPath === href.toString(), [currentPath, href]);
-
-  // 現在のクエリパラメータを取得します。
-  // @see https://nextjs.org/docs/app/api-reference/functions/use-search-params
-  const searchParams = useSearchParams();
-
-  const hrefWithSearchParamsPreserved = useMemo<string>(() => {
-    const searchParamsString = new URLSearchParams(searchParams).toString();
-    const encodedSearchParamsString = searchParamsString.replace(/%2C/g, ',');
-    return encodedSearchParamsString ? `${href}?${encodedSearchParamsString}` : href.toString();
-  }, [href, searchParams]);
+  const isBeingOpened = useIsBeingOpened(href.toString());
+  const hrefWithSearchParamsPreserved = useHrefWithPreservedSearchParams(href.toString());
 
   const link = topNavigationLinkRecipe({
     selected: selected || isBeingOpened,
