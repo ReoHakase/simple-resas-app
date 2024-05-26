e07c082567962b0e3c88d51f7653cf87073ddf88
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-05-26 20:45:11 +0900

    build: 📦️ (empty-package) 未使用の内部パッケージを削除した。  (#36)

--- packages/empty-package/.lintstagedrc.js
+++
@@ -1,6 +0,0 @@
-// This file is used by lint-staged to run linting and formatting on staged files
-
-module.exports = {
-  '**/*.{js,jsx,cjs,mjs,ts,tsx}': 'pnpm eslint',
-  '**/*.{js,jsx,cjs,mjs,ts,tsx,md,html,css,json,yaml,yml}': 'pnpm prettier --ignore-unknown --check',
-};
