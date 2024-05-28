9c58cf991c8273eb7cf5459123b04dea9bb61ca8
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-05-28 14:27:13 +0900

    refactor: ♻️ (web) フォントの事前読み込みを無効化した。  (#32)

--- apps/web/src/styles/fonts/index.ts
+++ apps/web/src/styles/fonts/index.ts
@@ -4,6 +4,7 @@ export const calSans = localFont({
   src: './CalSans-SemiBold.woff2',
   display: 'swap',
   variable: '--font-cal-sans',
+  preload: false,
 });
 
 export const fontVariables = `${calSans.variable}`;
