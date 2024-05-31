083afebf3244844c160f0c480ed72c5de1582350
Reo Hakuta <st20152ro@gm.ibaraki-ct.ac.jp>
2024-05-31 18:37:32 +0900

    build: 📦️ `husky`と`lint-staged`を削除した。  (#51)

--- packages/tsconfig/.lintstagedrc.js
+++
@@ -1,5 +0,0 @@
-// This file is used by lint-staged to run linting and formatting on staged files
-
-module.exports = {
-  '**/*.{js,jsx,ts,tsx,md,html,css,json,yaml,yml}': 'pnpm prettier --ignore-unknown --check',
-};
